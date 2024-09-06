dd2aab2551bc8d27498b0c2508de63392766e178
Abubakrkazi <kaziabubakr87@gmail.com>
2024-09-06 23:02:44 +0600

    first commit

---
+++ logical_oper.js
@@ -0,0 +1,23 @@
+//...&& ....use
+var salary=50K;
+ var isBcs=true;
+ var weight=50;
+ if(salary>30 && weight>40){
+    console.log('We want this patro');
+ }
+ else{
+    console.log(Canceled);
+ }
+
+//...||...use
+var salary=50K;
+ var isBcs=true;
+ var weight=50;
+ if(salary>30 || weight>40){
+    console.log('We want this patro');
+ }
+ else{
+    console.log(Canceled);
+ }
+
+
