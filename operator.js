44ffe311f3a530f7c0aebd3955bae3efe37cedf8
Abubakrkazi <kaziabubakr87@gmail.com>
2024-09-06 22:30:00 +0600

    4th commit

---
+++ operator.js
@@ -0,0 +1,18 @@
+// ....Addition ...
+var price=50;
+//price=price+50;//price+=10;
+price+=10;
+console.log(price);
+
+// ...Substrction..
+
+var price=50;
+//price=price-50;//price-=10;
+price-=10;
+console.log(price);
+
+// ...Multiply...
+var price=50;
+//price=price*50;//price*J=10;
+price*=10;
+console.log(price);
