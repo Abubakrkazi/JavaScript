f77f0a5ef5a9314bf24811373be3a19312e0e636
Abubakrkazi <kaziabubakr87@gmail.com>
2024-09-06 23:37:09 +0600

    first commit

---
+++ arry.js
@@ -0,0 +1,9 @@
+const numbers=[80.90,100,150,200,250,300];
+console.log(numbers);
+
+// show any addresses value
+console.log(numbers[2]);
+
+//...set update any value in any address
+numbers[2]=5000;
+console.log(numbers);
