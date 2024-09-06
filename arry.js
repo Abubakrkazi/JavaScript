e28458d7af10fbb87b7e0dcdf099d35c3f83ec51
Abubakrkazi <kaziabubakr87@gmail.com>
2024-09-07 00:02:29 +0600

    2nd commit

--- arry.js
+++ arry.js
@@ -7,3 +7,32 @@ console.log(numbers[2]);
 //...set update any value in any address
 numbers[2]=5000;
 console.log(numbers);
+
+
+
+
+// ..... Add value in arry;
+numbers.push(700);
+console.log(numbers);
+
+//Delete last value in arry
+numbers.pop();
+console.log(numbers);
+
+// Delte value in arry at first position
+numbers.shift();
+console.log(numbers);
+
+//Add value in arry at first position
+numbers.unshift("");
+console.log(numbers);
+
+// kono elemenst ase kina dekte
+// includse - case sencitive 
+ console.log(numbers.includes(700));
+
+ // kono  element er index dekar jonno-indexOf
+ console.log(numbers.indexOf('....'));
+
+ //Arry seck-isArry
+ console.log(Array.isArray(numbers));
