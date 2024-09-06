1b306db0608e3df3bc63510fb804b7a62f8c0586
Abubakrkazi <kaziabubakr87@gmail.com>
2024-09-06 23:14:53 +0600

    first commit

---
+++ ternaryifelse.js
@@ -0,0 +1,11 @@
+// Ternary if- else er use ;
+// .......?....  :  ....
+// condition  ? do someting when ture : do something when false
+var age=20;
+if(age>=18){
+    console.log("You can vot");
+}
+else{
+    console.log('Not Allowe');
+}
+// age>=18  ? console.log("You can vot"):console.log('Not Allowe')
