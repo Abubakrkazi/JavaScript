0f04102850c1ffed460461e745401978b3b5dd29
Abubakrkazi <kaziabubakr87@gmail.com>
2024-09-06 22:45:36 +0600

    2nd commit

--- compar.js
+++ compar.js
@@ -1,2 +1,8 @@
+//==,===
 console.log(20=="20");//just value check;
 console.log(20==="20");//value and type;
+
+
+//  = ,==
+console.log(20!="20");//just value check;
+console.log(20!=="20");//value and type;
