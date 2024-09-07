ccd63040ed6f3e4f78125234c10883062df07102
Abubakrkazi <kaziabubakr87@gmail.com>
2024-09-07 11:24:11 +0600

    First commit

---
+++ loop.js
@@ -0,0 +1,19 @@
+//..while loop..
+let num=0;
+while(num<=10){
+    console.log('looping',num);
+ num++;
+}
+//...For loop...
+for(let num=0;num<10;num++){
+    console.log('loooping',num);
+}
+
+// Do while loop
+
+let  num=0;
+do{
+    console.log(num);
+    num++;
+}
+while(num<10)
