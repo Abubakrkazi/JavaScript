f1f02f4ae00eb27b5e8b94e77f67a3fe256878cb
Abubakrkazi <kaziabubakr87@gmail.com>
2024-09-07 13:42:42 +0600

    5th commit

--- ovject.js
+++ ovject.js
@@ -22,3 +22,14 @@ for(const prop in person){
     console.log(prop)
     console.log(person[prop])
 }
+
+
+// Sorting arry
+const numbers=[2,6,8,10,90,70,50,30];
+const num_asc=numbers.sort(function(a,b){return a-b})
+console.log(num_asc);
+
+// soting decending order
+const numbers=[2,6,8,10,90,70,50,30];
+const num_dsc=numbers.sort(function(a,b){return b-a})
+console.log(num_dsc);
